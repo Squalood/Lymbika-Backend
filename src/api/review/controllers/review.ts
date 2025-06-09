be11d7b9f25834fd9245dc04bb26986250a60202
Squalood <adriancas98@gmail.com>
2025-06-08 23:33:20 -0600

    fetch: forzar registrar usuario en review

--- src/api/review/controllers/review.ts
+++ src/api/review/controllers/review.ts
@@ -4,4 +4,24 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::review.review');
+export default factories.createCoreController("api::review.review", ({ strapi }) => ({
+  async create(ctx) {
+    const user = ctx.state.user;
+
+    if (!user) {
+      return ctx.unauthorized("No puedes crear una reseña sin estar logueado.");
+    }
+
+    const { data } = ctx.request.body;
+
+    const response = await strapi.entityService.create("api::review.review", {
+      data: {
+        ...data,
+        user: user.id, // Asignar manualmente
+      },
+    });
+
+    return response;
+  },
+}));
+
