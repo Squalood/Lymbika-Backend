f3668b62c684006f577591c1348d3f7a904ec80f
Squalood <adriancas98@gmail.com>
2025-06-12 12:39:04 -0600

    fetch: stripe deliveyBuy & pickUpBuy

--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -1,5 +1,7 @@
 "use strict";
 
+import Stripe from "stripe";
+
 //@ts-ignore
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
@@ -11,7 +13,7 @@ const { createCoreController } = require("@strapi/strapi").factories;
 module.exports = createCoreController("api::order.order", ({ strapi }) => ({
     async create(ctx) {
         //@ts-ignore
-        const { products, mediClubRegular } = ctx.request.body;
+        const { products, mediClubRegular, isDelivery } = ctx.request.body;
 
         try {
             const lineItems = await Promise.all(
@@ -31,14 +33,19 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
                 })
             );
 
-            const session = await stripe.checkout.sessions.create({
-                shipping_address_collection: { allowed_countries: ["MX"] }, 
+            const sessionConfig: Stripe.Checkout.SessionCreateParams = {
                 payment_method_types: ["card"],
                 mode: "payment",
-                success_url: process.env.CLIENT_URL + "/success", 
+                success_url: process.env.CLIENT_URL + "/success",
                 cancel_url: process.env.CLIENT_URL + "/cart",
                 line_items: lineItems,
-            });
+            };
+
+            if (isDelivery) {
+                sessionConfig.shipping_address_collection = { allowed_countries: ["MX"] };
+            }
+
+            const session = await stripe.checkout.sessions.create(sessionConfig)
 
             await strapi.service("api::order.order").create({
                 data: { products, stripeid: session.id },
