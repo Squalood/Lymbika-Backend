4fb56b33143db20d7e8eaec2e9bacb702de1813f
Squalood <adriancas98@gmail.com>
2025-06-20 10:38:32 -0600

    rework: restaurar a copia de seguridad

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,18 +1,3 @@
-import type { Schema, Struct } from '@strapi/strapi';
-
-export interface MondayDay extends Struct.ComponentSchema {
-  collectionName: 'components_monday_days';
-  info: {
-    displayName: 'day';
-    icon: '';
-  };
-  attributes: {};
-}
-
-declare module '@strapi/strapi' {
-  export module Public {
-    export interface ComponentSchemas {
-      'monday.day': MondayDay;
-    }
-  }
-}
+/*
+ * The app doesn't have any components yet.
+ */
